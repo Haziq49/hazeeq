69ff4f04c527f426977e7e327477a5990f725930
Haziq49 <iqziq0043@gmail.com>
2025-01-09 21:54:48 +0800

    Add files via upload

---
+++ WebDesign/scripts.js
@@ -0,0 +1,11 @@
+document.getElementById("contactForm")?.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thank you for reaching out! We'll get back to you soon.");
+});
+
+document.getElementById("loginForm")?.addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("You have successfully logged in");
+});
+
+
